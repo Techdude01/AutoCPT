aadda307cfeb5c324a9d13f519598912bad28f1b
yp583 <pately583@gmail.com>
2025-02-08 17:06:40 -0500

    send to gpt

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from flask_sock import Sock
 import json
 from dotenv import load_dotenv
 import os
-
+from test_groq import get_cpt_codes
+from datetime import datetime
 load_dotenv()
 
 app = Flask(__name__)
@@ -14,24 +15,29 @@ sock = Sock(app)
 cpt_history = []
 
 full_transcription = ""
+transcription_start_time = None
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 def generate_cpt_codes(transcribed_text):
-    global full_transcription
+    global full_transcription, transcription_start_time
     full_transcription += transcribed_text.lower()
-    cpt_dict = {
-        "consultation": "99241",
-        "physical examination": "99386",
-        "surgery": "10140",
-        "therapy": "97110",
-        "injection": "96372"
-    }
-
-    detected_codes = {word: cpt_dict[word] for word in cpt_dict if word in transcribed_text.lower()}
-    return detected_codes if detected_codes else {}
+    # cpt_dict = {
+    #     "consultation": "99241",
+    #     "physical examination": "99386",
+    #     "surgery": "10140",
+    #     "therapy": "97110",
+    #     "injection": "96372"
+    # }
+    if transcription_start_time is None:
+        transcription_start_time = datetime.now()
+    if (datetime.now() - transcription_start_time).seconds > 5:
+        detected_codes = get_cpt_codes(full_transcription)
+        transcription_start_time = datetime.now()
+        return detected_codes if detected_codes else {}
+    return {}
 
 @sock.route('/ws')
 def websocket_connection(ws):
